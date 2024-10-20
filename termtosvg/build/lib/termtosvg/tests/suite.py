4a9b1eeb4215af992fa2317894d2a28c00eb60e8
Sebastian <peraltas@seas.upenn.edu>
2024-10-20 05:19:56 -0400

    latest

--- termtosvg/build/lib/termtosvg/tests/suite.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-from termtosvg.tests.test_anim import TestAnim
-from termtosvg.tests.test_asciicast import TestAsciicast
-from termtosvg.tests.test_config import TestConf
-from termtosvg.tests.test_main import TestMain
-from termtosvg.tests.test_term import TestTerm
